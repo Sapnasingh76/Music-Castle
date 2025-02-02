7f947e6c7d3880cb665fff48149ae1c41e24483a
Sapnasingh76 <ssapnakumari234@gmail.com>
2025-02-02 09:21:06 +0530

    thanks

---
+++ songs.js
@@ -0,0 +1,7 @@
+let songs = [
+    { songName: "Die with a smile", filepath: "blue/1.mp3", coverpath: " blue/die.jpg" },
+    //{songName:"Thousand years",  filepath :"song/2.mp3",coverpath :"covers/2.jpg"},
+    //{songName:"Die with a smile",filepath :"song/3.mp3",coverpath :"covers/3.jpg"},
+    //{songName:"Die with a smile",filepath :"song/4.mp3",coverpath :"covers/4.jpg"},
+    //{songName:"Die with a smile",filepath :"song/5.mp3",coverpath :"covers/5.jpg"},
+];
